addd0c21733ab87c0c44e24d95dca5a8b34fb282
Frédéric Barthelet <fredericb@theodo.fr>
2022-05-20 14:54:17 +0200

    Initial commit

---
+++ src/cli.ts
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import { toto } from './index';
+import { program } from 'commander'
+
+program
+    .version('0.0.1')
+  .option('-p, --peppers', 'Add peppers')
+  .option('-P, --pineapple', 'Add pineapple')
+  .option('-b, --bbq-sauce', 'Add bbq sauce')
+  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
+  .parse(process.argv)
+
+  toto();
---
+++ src/index.ts
@@ -0,0 +1,3 @@
+export const toto = () => {
+    console.log("Hello !")
+}
