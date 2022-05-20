9d6aa83337a123dff29222449693ffb9405e32bd
Frédéric Barthelet <fredericb@theodo.fr>
2022-05-20 18:24:48 +0200

    Add prettier and eslint

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,17 @@
 #!/usr/bin/env node
+import { program } from 'commander';
 import { toto } from './index';
-import { program } from 'commander'
 
 program
-    .version('0.0.1')
+  .version('0.0.1')
   .option('-p, --peppers', 'Add peppers')
   .option('-P, --pineapple', 'Add pineapple')
   .option('-b, --bbq-sauce', 'Add bbq sauce')
-  .option('-c, --cheese [type]', 'Add the specified type of cheese [marble]', 'marble')
-  .parse(process.argv)
+  .option(
+    '-c, --cheese [type]',
+    'Add the specified type of cheese [marble]',
+    'marble',
+  )
+  .parse(process.argv);
 
-  toto();
+toto();
