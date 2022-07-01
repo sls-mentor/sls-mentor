b81ef16d8d2188197afc853d174f51258b4ad873
PChol22 <pierrech@theodo.fr>
2022-07-01 18:02:37 +0200

    create a cache middleware to send only one uniq request per resource for each request type

---
+++ src/clients/index.ts
@@ -0,0 +1,1 @@
+export { default as lambdaClient } from './lambdaClient';
--- src/helpers/fetchLambdaVersion.ts
+++ src/helpers/fetchLambdaVersion.ts
@@ -1,17 +1,16 @@
 import {
   FunctionConfiguration,
-  LambdaClient,
   ListVersionsByFunctionCommand,
 } from '@aws-sdk/client-lambda';
 import { ARN, build } from '@aws-sdk/util-arn-parser';
 import { Resource } from '../types';
 import { filterLambdaFromResources } from './filterLambdaFromResources';
+import { lambdaClient } from '../clients';
 
 const fetchLambdaVersionsByArn = async (
   arn: ARN,
-  client: LambdaClient,
 ): Promise<FunctionConfiguration[]> => {
-  const { Versions: versions } = await client.send(
+  const { Versions: versions } = await lambdaClient.send(
     new ListVersionsByFunctionCommand({ FunctionName: arn.resource }),
   );
 
@@ -21,13 +20,12 @@ const fetchLambdaVersionsByArn = async (
 export const fetchAllLambdaVersions = async (
   resources: Resource[],
 ): Promise<{ arn: string; versions: FunctionConfiguration[] }[]> => {
-  const client = new LambdaClient({});
   const lambdaResources = filterLambdaFromResources(resources);
 
   const lambdaVersions = await Promise.all(
     lambdaResources.map(async ({ arn }) => ({
       arn: build(arn),
-      versions: await fetchLambdaVersionsByArn(arn, client),
+      versions: await fetchLambdaVersionsByArn(arn),
     })),
   );
 
