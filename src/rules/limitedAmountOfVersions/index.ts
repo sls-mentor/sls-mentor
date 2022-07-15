90ac990ffca490e1b98e57f1e9b07603b8819b41
PChol22 <pierrech@theodo.fr>
2022-07-15 18:41:14 +0200

    refactor(Rules): add all rules to readme + update rules name + update rules readme

--- src/rules/limitedAmountOfVersions/index.ts
+++ src/rules/limitedAmountOfVersions/index.ts
@@ -7,22 +7,22 @@ import {
   RuleDisplayNames,
 } from '../../types';
 
-const MAX_NUMBER_OF_VERSIONS = 3 + 1; // +$latest
+const MAX_AMOUNT_OF_VERSIONS = 3 + 1; // +$latest
 
 const run = async (resources: ARN[]): Promise<{ results: CheckResult[] }> => {
   const lambdaVersions = await fetchAllLambdaVersions(resources);
 
   const results = lambdaVersions.map(({ arn, versions }) => ({
     arn,
-    success: versions.length <= MAX_NUMBER_OF_VERSIONS,
-    versionNumber: versions.length,
+    success: versions.length <= MAX_AMOUNT_OF_VERSIONS,
+    versionAmount: versions.length,
   }));
 
   return { results };
 };
 
 export default {
-  ruleName: RuleDisplayNames.LIMITED_NUMBER_OF_LAMBDA_VERSIONS,
-  errorMessage: ErrorMessages.LIMITED_NUMBER_OF_LAMBDA_VERSIONS,
+  ruleName: RuleDisplayNames.LIMITED_AMOUNT_OF_LAMBDA_VERSIONS,
+  errorMessage: ErrorMessages.LIMITED_AMOUNT_OF_LAMBDA_VERSIONS,
   run,
 } as Rule;
