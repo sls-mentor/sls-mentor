6a214e5d770d169299d028b0131c0574a0868da8
PChol22 <pierrech@theodo.fr>
2022-07-29 18:30:57 +0200

    fix - fetchCloudFormation Bug

--- src/helpers/fetchCloudFormationResourceArns.ts
+++ src/helpers/fetchCloudFormationResourceArns.ts
@@ -44,7 +44,7 @@ export const fetchCloudFormationResourceArns = async (
       ? getSupportedResourceArn(
           { ResourceType, PhysicalResourceId },
           region,
-          'Account',
+          Account,
         )
       : [],
   );
