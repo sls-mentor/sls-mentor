cc2af23a14a05eb47b56846fe2723561c55d284f
Adele Gauvrit <gauvrit.adele@gmail.com>
2022-08-12 17:41:57 +0200

    Rename delay constant

--- src/clients/standardRetryStrategy.ts
+++ src/clients/standardRetryStrategy.ts
@@ -1,13 +1,13 @@
 import { StandardRetryStrategy } from '@aws-sdk/middleware-retry';
 
 export const MAXIMUM_ATTEMPTS = 10;
-const DELAY_RATIO = 1000;
+const DELAY_INCREMENT = 1000;
 
 export const standardRetryStrategy = new StandardRetryStrategy(
   () => Promise.resolve(MAXIMUM_ATTEMPTS),
   {
     delayDecider: (_delayBase, attempts) => {
-      return DELAY_RATIO * attempts;
+      return DELAY_INCREMENT * attempts;
     },
   },
 );
