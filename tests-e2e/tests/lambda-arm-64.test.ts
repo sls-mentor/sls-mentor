9f6837b4d3b096c27e9637667cd96efff6ba1240
valentinbeggi <valentinbe@theodo.fr>
2022-11-18 17:56:21 +0100

    :white_check_mark: add arm64 test

---
+++ tests-e2e/tests/lambda-arm-64.test.ts
@@ -0,0 +1,25 @@
+import { describe, expect, it } from 'vitest';
+import UseArmRule from '../../src/rules/useArm';
+import { FAIL_ARM64_LAMBDA_NAME } from '../lib/failStack/lambda';
+import { PASS_ARM64_LAMBDA_NAME } from '../lib/passStack/lambda';
+import { guardianResult } from './testSetup/guardianResult';
+
+const ruleName = UseArmRule['ruleName'];
+
+const guardianOutput = guardianResult[ruleName];
+
+describe('lambda-arm-64', () => {
+  it('guardian passes on lambda with ARM64 architecture ', () => {
+    const { result } = guardianOutput;
+    expect(
+      result.find(r => r.arn.includes(PASS_ARM64_LAMBDA_NAME))?.success,
+    ).toBe(true);
+  });
+
+  it('guardian fails on lambda with X86 architecture', () => {
+    const { result } = guardianOutput;
+    expect(
+      result.find(r => r.arn.includes(FAIL_ARM64_LAMBDA_NAME))?.success,
+    ).toBe(false);
+  });
+});
