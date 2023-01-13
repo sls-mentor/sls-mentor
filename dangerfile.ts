7d5f36676e8b4745e3f3d9542909f24bd1e6f154
Adele Gauvrit <gauvrit.adele@gmail.com>
2023-01-13 18:38:29 +0100

    ♻️ Create test to check each new rule has documentation

--- dangerfile.ts
+++ dangerfile.ts
@@ -1,4 +1,42 @@
-import { danger, message } from 'danger';
+import { danger, fail, message } from 'danger';
 
-const modifiedMD = danger.git.modified_files.join('- ');
-message('Changed Files in this PR: \n - ' + modifiedMD);
+const createdFiles = danger.git.created_files;
+
+const newRules: Record<string, boolean | undefined> = {};
+createdFiles.forEach(file => {
+  console.log(file);
+  if (!file.match(/packages\/core\/src\/rules\//)) {
+    return;
+  }
+  const rulePath = file.split('/');
+
+  const ruleName = rulePath[4];
+  if (newRules[ruleName] === undefined) {
+    newRules[ruleName] = false;
+
+    return;
+  }
+});
+
+createdFiles.forEach(file => {
+  if (!file.match(/^apps\/documentation\/docs\/rules\/.*\.md$/)) {
+    return;
+  }
+  const rulePath = file.split('/');
+
+  const ruleName = rulePath[4].split('.')[0];
+  if (newRules[ruleName] === false) {
+    newRules[ruleName] = true;
+
+    return;
+  }
+});
+
+Object.keys(newRules).map(newRuleName => {
+  if (newRules[newRuleName] === false) {
+    fail(`New rule "${newRuleName}" is missing documentation 👿`);
+
+    return;
+  }
+  message(`New rule "${newRuleName}" was created with documentation 🎉`);
+});
