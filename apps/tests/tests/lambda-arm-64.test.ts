0d9def4e9a17b6f0beb98854b43370afc01d8341
valentinbeggi <valentinbe@theodo.fr>
2023-01-27 17:38:26 +0100

    :recycle: refacto test assertion

--- apps/tests/tests/lambda-arm-64.test.ts
+++ apps/tests/tests/lambda-arm-64.test.ts
@@ -1,27 +1,25 @@
 import { UseArm as UseArmRule } from '@sls-mentor/core';
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import { FAIL_ARM64_LAMBDA_NAME } from '../lib/failStack/lambda';
 import { PASS_ARM64_LAMBDA_NAME } from '../lib/passStack/lambda';
-import { slsMentorResult } from './testSetup/slsMentorResult';
+import { assertResourceResult } from './utils/assertResourceResult';
 
 const ruleName = UseArmRule['ruleName'];
 
-const slsMentorOutput = slsMentorResult[ruleName];
-
 describe('lambda-arm-64', () => {
   it('sls-mentor passes on lambda with ARM64 architecture ', () => {
-    const { result } = slsMentorOutput;
-    expect(
-      result.find(r => r.arn.resource.includes(PASS_ARM64_LAMBDA_NAME))
-        ?.success,
-    ).toBe(true);
+    assertResourceResult({
+      ruleName,
+      resourceName: PASS_ARM64_LAMBDA_NAME,
+      expectedResultForResource: true,
+    });
   });
 
   it('sls-mentor fails on lambda with X86 architecture', () => {
-    const { result } = slsMentorOutput;
-    expect(
-      result.find(r => r.arn.resource.includes(FAIL_ARM64_LAMBDA_NAME))
-        ?.success,
-    ).toBe(false);
+    assertResourceResult({
+      ruleName,
+      resourceName: FAIL_ARM64_LAMBDA_NAME,
+      expectedResultForResource: false,
+    });
   });
 });
--- apps/tests/tests/testSetup/globalSetup.ts
+++ apps/tests/tests/testSetup/globalSetup.ts
@@ -1,6 +1,10 @@
 import { config } from 'dotenv';
 import { unlinkSync, writeFileSync } from 'fs';
 import { getSlsMentorResult } from './getSlsMentorResults';
+import {
+  SLS_MENTOR_E2E_STACK_FAIL_NAME,
+  SLS_MENTOR_E2E_STACK_PASS_NAME,
+} from '../../bin/sls-mentor-e2e-app';
 
 export const setup = async (): Promise<void> => {
   config({ path: '.env' });
@@ -12,6 +16,10 @@ export const setup = async (): Promise<void> => {
     short: false,
     getJsonResults: true,
     level: '5',
+    cloudformationStacks: [
+      SLS_MENTOR_E2E_STACK_PASS_NAME,
+      SLS_MENTOR_E2E_STACK_FAIL_NAME,
+    ],
   });
 
   writeFileSync('./tests/slsMentorOutput.json', JSON.stringify(checkResults));
