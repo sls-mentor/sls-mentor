051ec118e6604d6a5df01d19b411b0149c867fa6
PChol22 <pierrech@theodo.fr>
2023-03-15 01:12:22 +0100

    feat(Front): add disclaimer message in report page

--- apps/documentation/src/pages/report/index.tsx
+++ apps/documentation/src/pages/report/index.tsx
@@ -5,6 +5,14 @@ import { Report } from '../../components';
 const ReportPage = (): JSX.Element => (
   <Layout title="Report" description="Your analysis details">
     <Report />
+    <p style={{ textAlign: 'center', color: 'var(--grey-low-contrast)' }}>
+      <u>Note</u>: This page was generated based on 100% anonymized data.
+      <br />
+      We only analyze rules passing percentages, your resources ARNs stay in
+      your CLI.
+      <br />
+      We DO NOT store any data anywhere.
+    </p>
   </Layout>
 );
 
