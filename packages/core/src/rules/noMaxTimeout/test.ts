02983bade8d698dc6d7e971dd0ed008ad7430948
vincentz <vincentz@theodo.fr>
2023-03-17 18:02:55 +0100

    ✅ add noMaxTimeout test

---
+++ packages/core/src/rules/noMaxTimeout/test.ts
@@ -0,0 +1,28 @@
+import { Duration } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { DefaultFunction } from '../../../tests/constructs';
+import { noMaxTimeout as NoMaxTimeoutRule } from './index';
+
+interface NoMaxTimeoutProps {
+  timeout?: Duration;
+}
+
+export class NoMaxTimeout extends Construct {
+  static passTestCases: Record<string, NoMaxTimeoutProps> = {
+    'no max timeout set': {
+      timeout: undefined,
+    },
+  };
+
+  static failTestCases: Record<string, NoMaxTimeoutProps> = {
+    'max timeout set to 10 min ': { timeout: Duration.seconds(900) },
+  };
+
+  constructor(scope: Construct, id: string, { timeout }: NoMaxTimeoutProps) {
+    super(scope, id);
+    const lambda = new DefaultFunction(this, 'LambdaFunction', {
+      timeout,
+    });
+    lambda.tagRule(NoMaxTimeoutRule);
+  }
+}
--- packages/core/tests/index.ts
+++ packages/core/tests/index.ts
@@ -3,6 +3,7 @@ export * from '../src/rules/cognitoSignInCaseInsensitivity/test';
 export * from '../src/rules/definedLogsRetentionDuration/test';
 export * from '../src/rules/enableBlockPublicAccess/test';
 export * from '../src/rules/noDefaultMemory/test';
+export * from '../src/rules/noMaxTimeout/test';
 export * from '../src/rules/noMonoPackage/test';
 export * from '../src/rules/snsRedrivePolicy/test';
 export * from '../src/rules/specifyDlqOnEventBridgeRule/test';
