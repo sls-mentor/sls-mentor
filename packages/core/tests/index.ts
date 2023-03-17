110fbbf8e39e53c2f684b9bf4a2f67f7fffa7e26
Hugo Brunet <hugo.brunet@grenoble-inp.org>
2023-03-17 17:21:28 +0100

    ✅ Add enableBlockPublicAccess test

--- packages/core/tests/index.ts
+++ packages/core/tests/index.ts
@@ -1,4 +1,5 @@
 export * from '../src/rules/definedLogsRetentionDuration/test';
+export * from '../src/rules/enableBlockPublicAccess/test';
 export * from '../src/rules/noDefaultMemory/test';
 export * from '../src/rules/noMonoPackage/test';
 export * from '../src/rules/useArm/test';
