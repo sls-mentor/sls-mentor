2e64ecf74164386973e65261802198c617ecc4e6
Juliette Fournier <juliettef@theodo.fr>
2023-03-31 16:37:14 +0200

    :white_check_mark: add test for EnforceLongPasswords Cognito rule

---
+++ packages/core/src/rules/cognitoEnforceLongPasswords/test.ts
@@ -0,0 +1,29 @@
+import { Construct } from 'constructs';
+import { cognitoEnforceLongPasswords } from './index';
+import { DefaultUserPool } from '../../../tests/constructs';
+
+interface EnforceLongPasswordsProps {
+  minLength: number;
+}
+
+export class EnforceLongPasswords extends Construct {
+  static passTestCases: Record<string, EnforceLongPasswordsProps> = {
+    'Password min lenth 10': { minLength: 10 },
+  };
+
+  static failTestCases: Record<string, EnforceLongPasswordsProps> = {
+    'Password min lenth 8': { minLength: 8 },
+  };
+
+  constructor(
+    scope: Construct,
+    id: string,
+    { minLength }: EnforceLongPasswordsProps,
+  ) {
+    super(scope, id);
+    const userPool = new DefaultUserPool(this, 'UserPool', {
+      passwordPolicy: { minLength },
+    });
+    userPool.tagRule(cognitoEnforceLongPasswords);
+  }
+}
