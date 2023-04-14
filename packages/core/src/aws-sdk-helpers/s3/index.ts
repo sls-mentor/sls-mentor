632b269b38e14a3b54dc78a3eab74a5fe7f8f0e2
Juliette Fournier <juliettef@theodo.fr>
2023-04-14 17:18:30 +0200

    :coffin: all buckets are encrypted since 01/2023, irrelevant rule

--- packages/core/src/aws-sdk-helpers/s3/index.ts
+++ packages/core/src/aws-sdk-helpers/s3/index.ts
@@ -1,4 +1,3 @@
-export * from './fetchS3BucketEncryptionConfiguration';
 export * from './fetchS3BucketLifecycleRules';
 export * from './fetchS3BucketPolicies';
 export * from './fetchPublicAccessBlockConfiguration';
