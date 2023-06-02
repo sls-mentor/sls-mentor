0a3d7709495e4bbb145f505b57593733d70b9d84
vincentz <vincentz@theodo.fr>
2023-06-02 17:39:05 +0200

    toto

--- .github/actions/sls-mentor-action-test/src/entrypoint.sh
+++ .github/actions/sls-mentor-action-test/src/entrypoint.sh
@@ -4,9 +4,9 @@ set -e
 
 echo "::debug::Set the Octocat variable"
 
-#[ -f yarn.lock ] && yarn install
-#[ -f package-lock.json ] && npm install
-#[ -f pnpm.lock ] && pnpm install
+[ -f yarn.lock ] && yarn install
+[ -f package-lock.json ] && npm install
+[ -f pnpm.lock ] && pnpm install
 
 NODE_PATH=node_modules GITHUB_TOKEN="${GITHUB_TOKEN:-${1:-.}}" node .github/actions/sls-mentor-action-test/dist/src/run.js --level $2
 
