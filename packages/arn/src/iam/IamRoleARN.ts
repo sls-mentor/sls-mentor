d40213fdf62f1e3caa4af8129b7973f857c80ac4
PChol22 <pierrech@theodo.fr>
2023-12-18 09:18:40 +0100

    fix(Iam): fix IamRole ARN

--- packages/arn/src/iam/IamRoleARN.ts
+++ packages/arn/src/iam/IamRoleARN.ts
@@ -6,13 +6,13 @@ export class IamRoleARN extends CustomARN {
   }
 
   static fromRoleName = (roleName: string): IamRoleARN =>
-    new IamRoleARN(`role:${roleName}`);
+    new IamRoleARN(`role/${roleName}`);
 
   static fromPhysicalId = (physicalId: string): IamRoleARN =>
     IamRoleARN.fromRoleName(physicalId);
 
   getRoleName = (): string => {
-    const roleName = this.resource.split(':')[1];
+    const roleName = this.resource.split('/')[1];
 
     if (roleName === undefined) {
       throw new Error('Invalid Iam Role ARN');
