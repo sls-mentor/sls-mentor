49d7b846a019ea79f186cf2af819458090a293df
PChol22 <pierrech@theodo.fr>
2024-01-21 17:00:17 +0100

    feat(3.0.0-front): improve error message when data is malformed

--- packages/graph-front/src/App.tsx
+++ packages/graph-front/src/App.tsx
@@ -7,7 +7,25 @@ const App = (): JSX.Element => {
   const graphData = useGraphData();
 
   if (graphData === undefined) {
-    return <div>Malformed ARN, could not load graph data</div>;
+    return (
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          height: '100vh',
+          gap: '1em',
+        }}
+      >
+        <p style={{ color: 'white', fontWeight: 600, fontSize: '2em' }}>
+          Could not load graph data.
+        </p>
+        <p style={{ color: 'white' }}>
+          Check if your filters matched at least one AWS resource.
+        </p>
+      </div>
+    );
   }
 
   return <LiveGraph data={graphData} />;
